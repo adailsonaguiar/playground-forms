99980ffe8bac6a1c2bf2ec4d2c6106da5445a9b8
unknown <adailsonacj@live.com>
2020-07-27 20:17:40 -0300

    update styles and validating values

--- src/App.js
+++ src/App.js
@@ -3,27 +3,44 @@ import './App.css';
 import { useFormValidator } from './hooks/form-validator';
 
 function App() {
-  const formValues = useFormValidator({
+  const formValidator = useFormValidator({
     initialValues: {
       userEmail: 'adailsonacj@live.com',
       userPassword: '123456',
+    },
+    validate: function (values) {
+      const errors = {};
+      if (!values.userEmail.includes('@'))
+        errors.userEmail = 'Please, insert a valid email';
+      return errors;
     },
   });
   return (
     <div className="App">
       <form>
-        <input
-          type="text"
-          name="userEmail"
-          value={formValues.values.userEmail}
-          onChange={formValues.handleChange}
-        />
+        <div className="formField">
+          <input
+            className={formValidator.errors.userEmail && 'invalid'}
+            type="text"
+            name="userEmail"
+            value={formValidator.values.userEmail}
+            onChange={formValidator.handleChange}
+          />
+          {formValidator.errors.userEmail && (
+            <span className="invalid-feedback">
+              {formValidator.errors.userEmail}
+            </span>
+          )}
+        </div>
         <input
           type="password"
           name="userPassword"
-          value={formValues.values.userPassword}
-          onChange={formValues.handleChange}
+          value={formValidator.values.userPassword}
+          onChange={formValidator.handleChange}
         />
+        {/* {formValidator.errors.userEmail && (
+          <span className="invalid-feedback">This field is required</span>
+        )} */}
         <button type="submit">PRÓXIMO</button>
       </form>
     </div>
--- src/hooks/form-validator.js
+++ src/hooks/form-validator.js
@@ -1,15 +1,25 @@
 import { useState } from 'react';
 
-export function useFormValidator({ initialValues }) {
+export function useFormValidator({ initialValues, validate }) {
   const [values, setValues] = useState(initialValues);
-
+  const [errors, setErrors] = useState({});
   function handleChange(e) {
     const fieldName = e.target.getAttribute('name');
     const value = e.target.value;
-    setValues({ ...values, [fieldName]: value });
+    const newValues = { ...values, [fieldName]: value };
+    setValues(newValues);
+    validateValues(newValues);
   }
+
+  function validateValues(values) {
+    console.log('validando...', validate(values));
+    setErrors(validate(values));
+  }
+
   return {
     handleChange,
     values,
+    errors,
+    setErrors,
   };
 }
